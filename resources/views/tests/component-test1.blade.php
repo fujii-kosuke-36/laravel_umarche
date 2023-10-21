0db74cfefc8395d7fe1a3d27e3618389666f67b8
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-10-21 15:43:21 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,4 +4,5 @@
 
     <x-tests.card title="タイトル" content="本文1" :message="$message" />
     <x-tests.card title="タイトル2" />
+    <x-tests.card title="CSSを変更したい" class="bg-green-500" />
 </x-tests.app>
