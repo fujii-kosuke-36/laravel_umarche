3a9d119151f28c28471714a9cb54ea5ea88ec723
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-10-21 14:56:37 +0900

    変数の受け渡し

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,6 @@
 <x-tests.app>
-    <x-slot name= "header">ヘッダー1</x-slot>
+    <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト１
 
-    <x-tests.card title="タイトル" content="本文1" />
+    <x-tests.card title="タイトル" content="本文1" :message="$message" />
 </x-tests.app>
