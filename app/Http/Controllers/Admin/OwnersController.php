64849ac864258080403b0029258acd63ee5fd1d6
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-10-23 15:38:55 +0900

    バリデーションエラー表示

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Owner;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Admin\Rules\Password;
 
 class OwnersController extends Controller
 {
@@ -59,7 +61,21 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $request->name
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'string', 'confirmed', 'min:8'],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('admin.owners.index');
+
     }
 
     /**
