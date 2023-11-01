a2a7a35a9badea2bba0ab37ae80eba3229cc5789
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-11-01 23:32:45 +0900

    image:store

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Image;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ImageController extends Controller
 {
@@ -32,7 +33,7 @@ class ImageController extends Controller
     public function index()
     {
         $images = Image::where('owner_id', Auth::id())
-        ->orderBy('update_at', 'desc')
+        ->orderBy('updated_at', 'desc')
         ->paginate(20);
 
         return view('owner.images.index', compact('images'));
@@ -56,7 +57,20 @@ class ImageController extends Controller
      */
     public function store(UploadImageRequest $request)
     {
-        //
+        $imageFiles = $request->file('files');
+        if(!is_null($imageFiles)){
+            foreach($imageFiles as $imageFIle){
+                $fileNameToStore = ImageService::upload($imageFIle, 'products');
+                Image::create([
+                    'owner_id' => Auth::id(),
+                    'filename' => $fileNameToStore,
+                ]);
+            }
+        }
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with(['message' => '画像登録を実施しました。', 'status' => 'info']);
     }
 
     /**
