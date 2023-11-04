3f967b3cb458aacef2e103ef42317894613e797f
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-11-04 21:38:44 +0900

    product store

--- database/migrations/2023_11_02_210401_create_products_table.php
+++ database/migrations/2023_11_02_210401_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text("information")
+            $table->text('information');
             $table->unsignedInteger('price');
             $table->boolean('is_selling');
             $table->integer('sort_order')->nullable();
