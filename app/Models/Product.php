6cc8330c01c294c6029856fe70cf6649d18fc4e6
fujii-kosuke-36 <ztpr9975@gmail.com>
2023-11-04 15:59:08 +0900

    Product雛形作成

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+use App\Models\Shop;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
+}
